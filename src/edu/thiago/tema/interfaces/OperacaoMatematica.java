dfd783c5c6401afdcdd96c0e0d9133cc630c566e
Thiago Bohonos <thiago.bohonos@gmail.com>
2023-01-27 12:16:54 -0300

    Aplicando interface

---
+++ src/edu/thiago/tema/interfaces/OperacaoMatematica.java
@@ -0,0 +1,10 @@
+package edu.thiago.tema.interfaces;
+
+public interface OperacaoMatematica {
+
+    void soma(double operando1, double operando2);
+    void subtracao(double operando1, double operando2);
+    void multiplicacao(double operando1, double operando2);
+    void divisao(double operando1, double operando2);
+
+}
