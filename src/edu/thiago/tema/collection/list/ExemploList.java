a4a6a4346b28a77f89b17884b52a05de981c95a3
Thiago Bohonos <thiago.bohonos@gmail.com>
2023-01-31 17:18:11 -0300

    Conhecendo e aplicando os exercícios com os métodos List

--- src/edu/thiago/tema/collection/list/ExemploList.java
+++ src/edu/thiago/tema/collection/list/ExemploList.java
@@ -1,7 +1,8 @@
 package edu.thiago.tema.collection.list;
 
-import javax.lang.model.type.NoType;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class ExemploList {
@@ -33,5 +34,43 @@ public class ExemploList {
 
         System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
         for (Double nota : notas) System.out.println(nota);
+
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a menor nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova a nota da posição 0: ");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
     }
 }
